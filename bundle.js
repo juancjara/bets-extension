4208c2adf207bf975a06d20bf076188ffe4adaf9
juancjara <juanc.jara@pucp.pe>
2015-05-05 10:19:42 -0500

    m

--- bundle.js
+++ bundle.js
@@ -12,15 +12,34 @@ function httpRequest(url, cb) {
   xhr.send();
 }
 
-function parseOngoingEvents(data) {
-  var ongoingEvents = parseData.FetchLiveEventsMatchWinnerJSONPResult.OngoingEvents;
+function ParseEvents(evt) {
+  evt.events.forEach(function(game) {
 
+    console.log(game.EventName);
+    console.log(game.EventPeriodName);
+    console.log(game.SubCategoryName);
+    //IsMatchClockRunning: true
+    var gameResults = game.GameResults;
+    console.log('score', gameResults[0].GameResultValue,
+                 gameResults[1].GameResultValue);
+    var teamsOdds = game.MarketGroups[0].Markets[0].MarketSelections;
+    teamsOdds.forEach(function(item) {
+      console.log(item.MarketSelectionName, item.Odds);
+    });
+  })
+}
+
+function parseOngoingEvents(data) {
+  var ongoingEvents = data.FetchLiveEventsMatchWinnerJSONPResult.OngoingEvents;
+  ongoingEvents.map(function(ev) {
+    return ParseEvents(ev);
+  })
 }
 
 $('#btn').on('click', function() {
   httpRequest(url, function(data) {
     var parseData = JSON.parse(data);
-    parseOngoingEvents(data);
+    parseOngoingEvents(parseData);
   });
 })
 
